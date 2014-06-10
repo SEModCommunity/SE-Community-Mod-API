d569cba29a953e77e71be07c2353235901b9a3d3
Joseph Pearson <chessmaster42@gmail.com>
2014-06-09 21:28:09 -0500

    -More code cleanup from merging weirdness

--- SEModAPI/API/ConfigFileSerializer.cs
+++ SEModAPI/API/ConfigFileSerializer.cs
@@ -126,38 +126,8 @@ namespace SEModAPI.API
 
         public MyObjectBuilder_CubeBlockDefinition[] CubeBlockDefinitions
         {
-            get
-            {
-                return _cubeBlockDefinitions.CubeBlocks;
-            }
+            get { return _cubeBlockDefinitions.CubeBlocks; }
         }
-
-        public void SetCubeBlockDefinitions(MyObjectBuilder_CubeBlockDefinition[] cubeBlockDefinitions)
-        {
-            _cubeBlockDefinitions.CubeBlocks = cubeBlockDefinitions;
-        }
-
-        public void SetCubeBlockDefinitionsIndex(int index, MyObjectBuilder_CubeBlockDefinition cubeBlockDefinition)
-        {
-            if (index < _cubeBlockDefinitions.CubeBlocks.Length)
-            {
-                _cubeBlockDefinitions.CubeBlocks[index] = cubeBlockDefinition;
-            } 
-        }
-
-        public void SetAmmoMagazinesDefinitions(MyObjectBuilder_AmmoMagazineDefinition[] ammoMagazinesDefinitions)
-        {
-            _ammoMagazineDefinitions.AmmoMagazines = ammoMagazinesDefinitions;
-        }
-
-        public void SetAmmoMagazinesDefinitionsIndex(int index, MyObjectBuilder_AmmoMagazineDefinition ammoMagazinesDefinition)
-        {
-            if (index < _ammoMagazineDefinitions.AmmoMagazines.Length)
-            {
-                _ammoMagazineDefinitions.AmmoMagazines[index] = ammoMagazinesDefinition;
-            }
-        }
-
 
         public MyBlockPosition[] CubeBlockPositions
         {
@@ -212,6 +182,33 @@ namespace SEModAPI.API
 		#endregion
 
 		#region "Setters"
+
+		public void SetCubeBlockDefinitions(MyObjectBuilder_CubeBlockDefinition[] cubeBlockDefinitions)
+		{
+			_cubeBlockDefinitions.CubeBlocks = cubeBlockDefinitions;
+		}
+
+		public void SetCubeBlockDefinitionsIndex(int index, MyObjectBuilder_CubeBlockDefinition cubeBlockDefinition)
+		{
+			if (index < _cubeBlockDefinitions.CubeBlocks.Length)
+			{
+				_cubeBlockDefinitions.CubeBlocks[index] = cubeBlockDefinition;
+			}
+		}
+
+		public void SetAmmoMagazinesDefinitions(MyObjectBuilder_AmmoMagazineDefinition[] ammoMagazinesDefinitions)
+		{
+			_ammoMagazineDefinitions.AmmoMagazines = ammoMagazinesDefinitions;
+		}
+
+		public void SetAmmoMagazinesDefinitionsIndex(int index, MyObjectBuilder_AmmoMagazineDefinition ammoMagazinesDefinition)
+		{
+			if (index < _ammoMagazineDefinitions.AmmoMagazines.Length)
+			{
+				_ammoMagazineDefinitions.AmmoMagazines[index] = ammoMagazinesDefinition;
+			}
+		}
+
 		#endregion
 
 		#region "Methods"
@@ -356,6 +353,218 @@ namespace SEModAPI.API
             throw new NotImplementedException(string.Format("SetCubeOrientation of type [{0}] not yet implemented.", type));
         }
 
+		#region FetchCubeBlockMass
+
+		public float FetchCubeBlockMass(MyObjectBuilderTypeEnum typeId, MyCubeSize cubeSize, string subTypeid)
+		{
+			float mass = 0;
+
+			var cubeBlockDefinition = GetCubeDefinition(typeId, cubeSize, subTypeid);
+
+			if (cubeBlockDefinition != null)
+			{
+				foreach (var component in cubeBlockDefinition.Components)
+				{
+					mass += _componentDefinitions.Components.Where(c => c.Id.SubtypeId == component.Subtype).Sum(c => c.Mass) * component.Count;
+				}
+			}
+
+			return mass;
+		}
+
+		public void AccumulateCubeBlueprintRequirements(string subType, MyObjectBuilderTypeEnum typeId, decimal amount, Dictionary<string, MyObjectBuilder_BlueprintDefinition.Item> requirements, out TimeSpan timeTaken)
+		{
+			TimeSpan time = new TimeSpan();
+			var bp = _blueprintDefinitions.Blueprints.FirstOrDefault(b => b.Result.SubtypeId == subType && b.Result.TypeId == typeId);
+			if (bp != null)
+			{
+				foreach (var item in bp.Prerequisites)
+				{
+					if (requirements.ContainsKey(item.SubtypeId))
+					{
+						// append existing
+						requirements[item.SubtypeId].Amount += (amount / bp.Result.Amount) * item.Amount;
+					}
+					else
+					{
+						// add new
+						requirements.Add(item.SubtypeId, new MyObjectBuilder_BlueprintDefinition.Item()
+						{
+							Amount = (amount / bp.Result.Amount) * item.Amount,
+							TypeId = item.TypeId,
+							SubtypeId = item.SubtypeId,
+							Id = item.Id
+						});
+					}
+
+					var ticks = TimeSpan.TicksPerSecond * (decimal)bp.BaseProductionTimeInSeconds * amount;
+					var ts = new TimeSpan((long)ticks);
+					time += ts;
+				}
+			}
+
+			timeTaken = time;
+		}
+
+		public MyObjectBuilder_DefinitionBase GetDefinition(MyObjectBuilderTypeEnum typeId, string subTypeId)
+		{
+			var cube = _cubeBlockDefinitions.CubeBlocks.FirstOrDefault(d => d.Id.TypeId == typeId && d.Id.SubtypeId == subTypeId);
+			if (cube != null)
+			{
+				return cube;
+			}
+
+			var item = _physicalItemDefinitions.PhysicalItems.FirstOrDefault(d => d.Id.TypeId == typeId && d.Id.SubtypeId == subTypeId);
+			if (item != null)
+			{
+				return item;
+			}
+
+			var component = _componentDefinitions.Components.FirstOrDefault(c => c.Id.TypeId == typeId && c.Id.SubtypeId == subTypeId);
+			if (component != null)
+			{
+				return component;
+			}
+
+			var magazine = _ammoMagazineDefinitions.AmmoMagazines.FirstOrDefault(c => c.Id.TypeId == typeId && c.Id.SubtypeId == subTypeId);
+			if (magazine != null)
+			{
+				return magazine;
+			}
+
+			return null;
+		}
+
+		public float GetItemMass(MyObjectBuilderTypeEnum typeId, string subTypeId)
+		{
+			var def = GetDefinition(typeId, subTypeId);
+			if (def is MyObjectBuilder_PhysicalItemDefinition)
+			{
+				var item2 = def as MyObjectBuilder_PhysicalItemDefinition;
+				return item2.Mass;
+			}
+
+			return 0;
+		}
+
+		public float GetItemVolume(MyObjectBuilderTypeEnum typeId, string subTypeId)
+		{
+			var def = GetDefinition(typeId, subTypeId);
+			if (def is MyObjectBuilder_PhysicalItemDefinition)
+			{
+				var item2 = def as MyObjectBuilder_PhysicalItemDefinition;
+				if (item2.Volume.HasValue)
+					return item2.Volume.Value;
+			}
+
+			return 0;
+		}
+
+		public IList<MyObjectBuilder_VoxelMaterialDefinition> GetMaterialList()
+		{
+			return _voxelMaterialDefinitions.VoxelMaterials;
+		}
+
+		public byte GetMaterialIndex(string materialName)
+		{
+			if (_materialIndex.ContainsKey(materialName))
+				return _materialIndex[materialName];
+			else
+			{
+				var material = _voxelMaterialDefinitions.VoxelMaterials.FirstOrDefault(m => m.Name == materialName);
+				var index = (byte)_voxelMaterialDefinitions.VoxelMaterials.ToList().IndexOf(material);
+				_materialIndex.Add(materialName, index);
+				return index;
+			}
+		}
+
+		public string GetMaterialName(byte materialIndex, byte defaultMaterialIndex)
+		{
+			if (materialIndex <= _voxelMaterialDefinitions.VoxelMaterials.Length)
+				return _voxelMaterialDefinitions.VoxelMaterials[materialIndex].Name;
+			else
+				return _voxelMaterialDefinitions.VoxelMaterials[defaultMaterialIndex].Name;
+		}
+
+		public string GetMaterialName(byte materialIndex)
+		{
+			return _voxelMaterialDefinitions.VoxelMaterials[materialIndex].Name;
+		}
+
+		public MyObjectBuilder_CubeBlockDefinition GetCubeDefinition(MyObjectBuilderTypeEnum typeId, MyCubeSize cubeSize, string subtypeId)
+		{
+			if (string.IsNullOrEmpty(subtypeId))
+			{
+				return _cubeBlockDefinitions.CubeBlocks.FirstOrDefault(d => d.CubeSize == cubeSize && d.Id.TypeId == typeId);
+			}
+
+			return _cubeBlockDefinitions.CubeBlocks.FirstOrDefault(d => d.Id.SubtypeId == subtypeId || (d.Variants != null && d.Variants.Any(v => subtypeId == d.Id.SubtypeId + v.Color)));
+			// Returns null if it doesn't find the required SubtypeId.
+		}
+
+		public BoundingBox GetBoundingBox(MyObjectBuilder_CubeGrid entity)
+		{
+			var min = new Vector3(int.MaxValue, int.MaxValue, int.MaxValue);
+			var max = new Vector3(int.MinValue, int.MinValue, int.MinValue);
+
+			foreach (var block in entity.CubeBlocks)
+			{
+				min.X = Math.Min(min.X, block.Min.X);
+				min.Y = Math.Min(min.Y, block.Min.Y);
+				min.Z = Math.Min(min.Z, block.Min.Z);
+				max.X = Math.Max(max.X, block.Min.X);       // TODO: resolve cubetype size.
+				max.Y = Math.Max(max.Y, block.Min.Y);
+				max.Z = Math.Max(max.Z, block.Min.Z);
+			}
+
+			// scale box to GridSize
+			var size = max - min;
+			if (entity.GridSizeEnum == MyCubeSize.Large)
+			{
+				size = new Vector3(size.X * 2.5f, size.Y * 2.5f, size.Z * 2.5f);
+			}
+			else if (entity.GridSizeEnum == MyCubeSize.Small)
+			{
+				size = new Vector3(size.X * 0.5f, size.Y * 0.5f, size.Z * 0.5f);
+			}
+
+			// translate box according to min/max, but reset origin.
+			var bb = new BoundingBox(new Vector3(0, 0, 0), size);
+
+			// TODO: translate for rotation.
+			//bb. ????
+
+			// translate position.
+			bb.Translate(entity.PositionAndOrientation.Value.Position);
+
+
+			return bb;
+		}
+
+		public string GetResourceName(string value)
+		{
+			if (value == null)
+				return null;
+
+			Sandbox.Common.Localization.MyTextsWrapperEnum myText;
+
+			if (Enum.TryParse<Sandbox.Common.Localization.MyTextsWrapperEnum>(value, out myText))
+			{
+				try
+				{
+					return Sandbox.Common.Localization.MyTextsWrapper.GetFormatString(myText);
+				}
+				catch
+				{
+					return value;
+				}
+			}
+
+			return value;
+		}
+
+		#endregion
+	
 		#endregion
 
 		#region ReadCubeBlocksDefinitions
@@ -512,217 +721,5 @@ namespace SEModAPI.API
         }
 
         #endregion
-
-        #region FetchCubeBlockMass
-
-        public float FetchCubeBlockMass(MyObjectBuilderTypeEnum typeId, MyCubeSize cubeSize, string subTypeid)
-        {
-            float mass = 0;
-
-            var cubeBlockDefinition = GetCubeDefinition(typeId, cubeSize, subTypeid);
-
-            if (cubeBlockDefinition != null)
-            {
-                foreach (var component in cubeBlockDefinition.Components)
-                {
-                    mass += _componentDefinitions.Components.Where(c => c.Id.SubtypeId == component.Subtype).Sum(c => c.Mass) * component.Count;
-                }
-            }
-
-            return mass;
-        }
-
-        public void AccumulateCubeBlueprintRequirements(string subType, MyObjectBuilderTypeEnum typeId, decimal amount, Dictionary<string, MyObjectBuilder_BlueprintDefinition.Item> requirements, out TimeSpan timeTaken)
-        {
-            TimeSpan time = new TimeSpan();
-            var bp = _blueprintDefinitions.Blueprints.FirstOrDefault(b => b.Result.SubtypeId == subType && b.Result.TypeId == typeId);
-            if (bp != null)
-            {
-                foreach (var item in bp.Prerequisites)
-                {
-                    if (requirements.ContainsKey(item.SubtypeId))
-                    {
-                        // append existing
-                        requirements[item.SubtypeId].Amount += (amount / bp.Result.Amount) * item.Amount;
-                    }
-                    else
-                    {
-                        // add new
-                        requirements.Add(item.SubtypeId, new MyObjectBuilder_BlueprintDefinition.Item()
-                        {
-                            Amount = (amount / bp.Result.Amount) * item.Amount,
-                            TypeId = item.TypeId,
-                            SubtypeId = item.SubtypeId,
-                            Id = item.Id
-                        });
-                    }
-
-                    var ticks = TimeSpan.TicksPerSecond * (decimal)bp.BaseProductionTimeInSeconds * amount;
-                    var ts = new TimeSpan((long)ticks);
-                    time += ts;
-                }
-            }
-
-            timeTaken = time;
-        }
-
-        public MyObjectBuilder_DefinitionBase GetDefinition(MyObjectBuilderTypeEnum typeId, string subTypeId)
-        {
-            var cube = _cubeBlockDefinitions.CubeBlocks.FirstOrDefault(d => d.Id.TypeId == typeId && d.Id.SubtypeId == subTypeId);
-            if (cube != null)
-            {
-                return cube;
-            }
-
-            var item = _physicalItemDefinitions.PhysicalItems.FirstOrDefault(d => d.Id.TypeId == typeId && d.Id.SubtypeId == subTypeId);
-            if (item != null)
-            {
-                return item;
-            }
-
-            var component = _componentDefinitions.Components.FirstOrDefault(c => c.Id.TypeId == typeId && c.Id.SubtypeId == subTypeId);
-            if (component != null)
-            {
-                return component;
-            }
-
-            var magazine = _ammoMagazineDefinitions.AmmoMagazines.FirstOrDefault(c => c.Id.TypeId == typeId && c.Id.SubtypeId == subTypeId);
-            if (magazine != null)
-            {
-                return magazine;
-            }
-
-            return null;
-        }
-
-        public float GetItemMass(MyObjectBuilderTypeEnum typeId, string subTypeId)
-        {
-            var def = GetDefinition(typeId, subTypeId);
-            if (def is MyObjectBuilder_PhysicalItemDefinition)
-            {
-                var item2 = def as MyObjectBuilder_PhysicalItemDefinition;
-                return item2.Mass;
-            }
-
-            return 0;
-        }
-
-        public float GetItemVolume(MyObjectBuilderTypeEnum typeId, string subTypeId)
-        {
-            var def = GetDefinition(typeId, subTypeId);
-            if (def is MyObjectBuilder_PhysicalItemDefinition)
-            {
-                var item2 = def as MyObjectBuilder_PhysicalItemDefinition;
-                if (item2.Volume.HasValue)
-                    return item2.Volume.Value;
-            }
-
-            return 0;
-        }
-
-        public IList<MyObjectBuilder_VoxelMaterialDefinition> GetMaterialList()
-        {
-            return _voxelMaterialDefinitions.VoxelMaterials;
-        }
-
-        public byte GetMaterialIndex(string materialName)
-        {
-            if (_materialIndex.ContainsKey(materialName))
-                return _materialIndex[materialName];
-            else
-            {
-                var material = _voxelMaterialDefinitions.VoxelMaterials.FirstOrDefault(m => m.Name == materialName);
-                var index = (byte)_voxelMaterialDefinitions.VoxelMaterials.ToList().IndexOf(material);
-                _materialIndex.Add(materialName, index);
-                return index;
-            }
-        }
-
-        public string GetMaterialName(byte materialIndex, byte defaultMaterialIndex)
-        {
-            if (materialIndex <= _voxelMaterialDefinitions.VoxelMaterials.Length)
-                return _voxelMaterialDefinitions.VoxelMaterials[materialIndex].Name;
-            else
-                return _voxelMaterialDefinitions.VoxelMaterials[defaultMaterialIndex].Name;
-        }
-
-        public string GetMaterialName(byte materialIndex)
-        {
-            return _voxelMaterialDefinitions.VoxelMaterials[materialIndex].Name;
-        }
-
-        public MyObjectBuilder_CubeBlockDefinition GetCubeDefinition(MyObjectBuilderTypeEnum typeId, MyCubeSize cubeSize, string subtypeId)
-        {
-            if (string.IsNullOrEmpty(subtypeId))
-            {
-                return _cubeBlockDefinitions.CubeBlocks.FirstOrDefault(d => d.CubeSize == cubeSize && d.Id.TypeId == typeId);
-            }
-
-            return _cubeBlockDefinitions.CubeBlocks.FirstOrDefault(d => d.Id.SubtypeId == subtypeId || (d.Variants != null && d.Variants.Any(v => subtypeId == d.Id.SubtypeId + v.Color)));
-            // Returns null if it doesn't find the required SubtypeId.
-        }
-
-        public BoundingBox GetBoundingBox(MyObjectBuilder_CubeGrid entity)
-        {
-            var min = new Vector3(int.MaxValue, int.MaxValue, int.MaxValue);
-            var max = new Vector3(int.MinValue, int.MinValue, int.MinValue);
-
-            foreach (var block in entity.CubeBlocks)
-            {
-                min.X = Math.Min(min.X, block.Min.X);
-                min.Y = Math.Min(min.Y, block.Min.Y);
-                min.Z = Math.Min(min.Z, block.Min.Z);
-                max.X = Math.Max(max.X, block.Min.X);       // TODO: resolve cubetype size.
-                max.Y = Math.Max(max.Y, block.Min.Y);
-                max.Z = Math.Max(max.Z, block.Min.Z);
-            }
-
-            // scale box to GridSize
-            var size = max - min;
-            if (entity.GridSizeEnum == MyCubeSize.Large)
-            {
-                size = new Vector3(size.X * 2.5f, size.Y * 2.5f, size.Z * 2.5f);
-            }
-            else if (entity.GridSizeEnum == MyCubeSize.Small)
-            {
-                size = new Vector3(size.X * 0.5f, size.Y * 0.5f, size.Z * 0.5f);
-            }
-
-            // translate box according to min/max, but reset origin.
-            var bb = new BoundingBox(new Vector3(0, 0, 0), size);
-
-            // TODO: translate for rotation.
-            //bb. ????
-
-            // translate position.
-            bb.Translate(entity.PositionAndOrientation.Value.Position);
-
-
-            return bb;
-        }
-
-        public string GetResourceName(string value)
-        {
-            if (value == null)
-                return null;
-
-            Sandbox.Common.Localization.MyTextsWrapperEnum myText;
-
-            if (Enum.TryParse<Sandbox.Common.Localization.MyTextsWrapperEnum>(value, out myText))
-            {
-                try
-                {
-                    return Sandbox.Common.Localization.MyTextsWrapper.GetFormatString(myText);
-                }
-                catch
-                {
-                    return value;
-                }
-            }
-
-            return value;
-		}
-
-		#endregion
 	}
 }
