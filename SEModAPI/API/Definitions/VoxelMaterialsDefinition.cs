b1454a003d47247ca6a50e70a788a063e578e57a
fvgoulet <flx890@gmail.com>
2014-06-11 02:34:30 -0400

    Doing some more fixes Update #4

--- SEModAPI/API/Definitions/VoxelMaterialsDefinition.cs
+++ SEModAPI/API/Definitions/VoxelMaterialsDefinition.cs
@@ -6,23 +6,16 @@ using SEModAPI.Support;
 
 namespace SEModAPI.API.Definitions
 {
-	public class VoxelMaterialsDefinition : BaseDefinition<MyObjectBuilder_VoxelMaterialDefinition>
+    public class VoxelMaterialsDefinition : OverLayerDefinition<MyObjectBuilder_VoxelMaterialDefinition>
 	{
 		#region "Constructors and Initializers"
 
-		public VoxelMaterialsDefinition(MyObjectBuilder_VoxelMaterialDefinition definition)
-			: base(definition)
-		{
-		}
+		public VoxelMaterialsDefinition(MyObjectBuilder_VoxelMaterialDefinition definition): base(definition)
+		{}
 
 		#endregion
 
 		#region "Properties"
-
-		public string Name
-		{
-			get { return m_definition.Name; }
-		}
 
 		public string MinedOre
 		{
@@ -30,64 +23,48 @@ namespace SEModAPI.API.Definitions
 		}
 
 		#endregion
+
+        #region "Methods"
+
+        protected override string GetNameFrom(MyObjectBuilder_VoxelMaterialDefinition definition)
+        {
+            return definition.Name;
+        }
+
+        #endregion 
 	}
 
-	public class VoxelMaterialDefinitionsWrapper : BaseDefinitionsWrapper<MyObjectBuilder_VoxelMaterialDefinition, VoxelMaterialsDefinition>
+    public class VoxelMaterialDefinitionsWrapper : OverLayerDefinitionsManager<MyObjectBuilder_VoxelMaterialDefinition, VoxelMaterialsDefinition>
 	{
 		#region "Constructors and Initializers"
 
-		public VoxelMaterialDefinitionsWrapper(MyObjectBuilder_VoxelMaterialDefinition[] definitions)
-			: base(definitions)
-		{
-		}
-
-		#endregion
-
-		#region "Properties"
-
-		new public bool Changed
-		{
-			get
-			{
-				foreach (var def in m_definitions)
-				{
-					if (def.Value.Changed)
-						return true;
-				}
-
-				return false;
-			}
-			set
-			{
-				base.Changed = value;
-			}
-		}
-
-		public MyObjectBuilder_VoxelMaterialDefinition[] RawDefinitions
-		{
-			get
-			{
-				MyObjectBuilder_VoxelMaterialDefinition[] temp = new MyObjectBuilder_VoxelMaterialDefinition[m_definitions.Count];
-				VoxelMaterialsDefinition[] definitionsArray = this.Definitions;
-
-				for (int i = 0; i < definitionsArray.Length; i++)
-				{
-					temp[i] = definitionsArray[i].Definition;
-				}
-
-				return temp;
-			}
-		}
+		public VoxelMaterialDefinitionsWrapper(MyObjectBuilder_VoxelMaterialDefinition[] definitions): base(definitions)
+		{}
 
 		#endregion
 
 		#region "Methods"
 
-		public void Save()
+        protected override VoxelMaterialsDefinition CreateOverLayerSubTypeInstance(MyObjectBuilder_VoxelMaterialDefinition definition)
+        {
+            return new VoxelMaterialsDefinition(definition);
+        }
+
+        protected override MyObjectBuilder_VoxelMaterialDefinition GetBaseTypeOf(VoxelMaterialsDefinition overLayer)
+        {
+            return overLayer.BaseDefinition;
+        }
+
+        protected override bool GetChangedState(VoxelMaterialsDefinition overLayer)
+        {
+            return overLayer.Changed;
+        }
+
+        protected override void Save()
 		{
 			if (!this.Changed) return;
 
-			m_configSerializer.VoxelMaterialDefinitions = this.RawDefinitions;
+			m_configSerializer.VoxelMaterialDefinitions = this.ExtractBaseDefinitions().ToArray();
 			m_configSerializer.SaveVoxelMaterialsContentFile();
 		}
 
