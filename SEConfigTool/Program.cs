fac4bd5bb43c7712e4dedd98ac7331527d680788
fvgoulet <flx890@gmail.com>
2014-06-18 22:11:49 -0400

    Added error handling at the base of the ConfigTool Update #33

--- SEConfigTool/Program.cs
+++ SEConfigTool/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using SEModAPI.Support;
 
 namespace SEConfigTool
 {
@@ -14,9 +12,22 @@ namespace SEConfigTool
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new SEConfigTool());
+	        try
+	        {
+		        Application.EnableVisualStyles();
+		        Application.SetCompatibleTextRenderingDefault(false);
+		        Application.Run(new SEConfigTool());
+	        }
+	        catch (AutoException eEx)
+	        {
+				MessageBox.Show(eEx.AdditionnalInfo + "\n\r" + eEx.GetDebugString(), @"SEConfigTool", MessageBoxButtons.OK, MessageBoxIcon.Error);
+		        throw;
+	        }
+	        catch (Exception ex)
+	        {
+				MessageBox.Show(ex.ToString() , @"SEConfigTool", MessageBoxButtons.OK, MessageBoxIcon.Error);
+		        throw;
+	        }
         }
     }
 }
