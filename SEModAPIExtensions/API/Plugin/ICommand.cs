5411e0a705891fd594612f8ab602bb4cecd99f4a
Geroge <Spartan322@live.com>
2014-07-15 11:31:11 -0400

    Create ICommand.cs

---
+++ SEModAPIExtensions/API/Plugin/ICommand.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SEModAPIExtensions.API.Plugin
+{
+	public interface ICommand
+	{
+		bool execute(string cmd, string[] args);
+		Object Owner
+		{ get; }
+		Object Executor
+		{ get; }
+	}
+}
