8207e06782aa06fce162b40cf585851086bf34d3
Geroge <Spartan322@live.com>
2014-07-15 11:35:22 -0400

    Create CommandBase.cs

---
+++ SEModAPIExtensions/API/Plugin/CommandBase.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SEModAPIExtensions.API.Plugin
+{
+	public abstract class CommandBase : ICommand
+	{
+	        #region "Attributes"
+	        
+	        
+	
+	}
+}
