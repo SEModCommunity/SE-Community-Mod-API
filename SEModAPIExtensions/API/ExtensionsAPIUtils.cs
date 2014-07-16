8072fcbe2cea4874ec99604452df778f79cd1cd5
Geroge <Spartan322@live.com>
2014-07-16 10:46:59 -0400

    Create ExtensionsAPIUtils.cs

---
+++ SEModAPIExtensions/API/ExtensionsAPIUtils.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace SEModAPIExtensions.API
+{
+	public class ExtensionsAPIUtils
+	{
+		#region "Attributes"
+		
+		
+		
+		#endregion
+		
+		#region "Constructors and Intializers"
+		
+		
+		
+		#endregion
+		
+		#region "Properties"
+		
+		
+		
+		#endregion
+		
+		#region "Methods"
+		
+		public LoadFile(Type lookfor)
+		{
+		
+		}
+		
+		#endregion
+	}
+}
