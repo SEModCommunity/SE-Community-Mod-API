a072aeb939d61bfd1795f3ebcdf11c9435138655
chessmaster42 <chessmaster42@gmail.com>
2014-07-21 18:31:13 -0500

    -Added files that got missed in last commit

---
+++ SEModAPIInternal/API/Entity/Sector/SectorObject/CubeGrid/CubeBlock/ProductionBlockEntity.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Sandbox.Common.ObjectBuilders;
+
+using SEModAPIInternal.API.Common;
+using SEModAPIInternal.Support;
+
+namespace SEModAPIInternal.API.Entity.Sector.SectorObject.CubeGrid.CubeBlock
+{
+	public class ProductionBlockEntity : FunctionalBlockEntity
+	{
+		#region "Attributes"
+
+		private InventoryEntity m_inputInventory;
+		private InventoryEntity m_outputInventory;
+
+		public static string ProductionBlockNamespace = "6DDCED906C852CFDABA0B56B84D0BD74";
+		public static string ProductionBlockClass = "F00C92FFA6F55CD283D86570FFC332AE";
+
+		public static string ProductionBlockGetInputInventoryMethod = "GetInventory";
+		public static string ProductionBlockGetOutputInventoryMethod = "GetInventory";
+
+		#endregion
+
+		#region "Constructors and Intializers"
+
+		public ProductionBlockEntity(CubeGridEntity parent, MyObjectBuilder_ProductionBlock definition)
+			: base(parent, definition)
+		{
+			m_inputInventory = new InventoryEntity(definition.InputInventory);
+			m_outputInventory = new InventoryEntity(definition.OutputInventory);
+		}
+
+		public ProductionBlockEntity(CubeGridEntity parent, MyObjectBuilder_ProductionBlock definition, Object backingObject)
+			: base(parent, definition, backingObject)
+		{
+			m_inputInventory = new InventoryEntity(definition.InputInventory, InternalGetInputInventory());
+			m_outputInventory = new InventoryEntity(definition.OutputInventory, InternalGetOutputInventory());
+		}
+
+		#endregion
+
+		#region "Properties"
+
+		[Category("Production Block")]
+		[Browsable(false)]
+		public InventoryEntity InputInventory
+		{
+			get
+			{
+				return m_inputInventory;
+			}
+		}
+
+		[Category("Production Block")]
+		[Browsable(false)]
+		public InventoryEntity OutputInventory
+		{
+			get
+			{
+				return m_outputInventory;
+			}
+		}
+
+		#endregion
+
+		#region "Methods"
+
+		#region "Internal"
+
+		protected Object InternalGetInputInventory()
+		{
+			try
+			{
+				Object baseObject = BackingObject;
+				Object actualObject = GetActualObject();
+				Object inventory = InvokeEntityMethod(actualObject, ProductionBlockGetInputInventoryMethod, new object[] { 0 });
+
+				return inventory;
+			}
+			catch (Exception ex)
+			{
+				LogManager.GameLog.WriteLine(ex);
+				return null;
+			}
+		}
+
+		protected Object InternalGetOutputInventory()
+		{
+			try
+			{
+				Object baseObject = BackingObject;
+				Object actualObject = GetActualObject();
+				Object inventory = InvokeEntityMethod(actualObject, ProductionBlockGetOutputInventoryMethod, new object[] { 1 });
+
+				return inventory;
+			}
+			catch (Exception ex)
+			{
+				LogManager.GameLog.WriteLine(ex);
+				return null;
+			}
+		}
+
+		#endregion
+
+		#endregion
+	}
+}
---
+++ SEModAPIInternal/API/Entity/Sector/SectorObject/CubeGrid/CubeBlock/ShipToolBaseEntity.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Game.Weapons;
+
+using SEModAPIInternal.API.Common;
+using SEModAPIInternal.Support;
+
+namespace SEModAPIInternal.API.Entity.Sector.SectorObject.CubeGrid.CubeBlock
+{
+	public class ShipToolBaseEntity : FunctionalBlockEntity
+	{
+		#region "Attributes"
+
+		private InventoryEntity m_inventory;
+
+		public static string ShipToolBaseNamespace = "Sandbox.Game.Weapons";
+		public static string ShipToolBaseClass = "MyShipToolBase";
+
+		public static string ShipToolBaseGetInventoryMethod = "GetInventory";
+
+		#endregion
+
+		#region "Constructors and Intializers"
+
+		public ShipToolBaseEntity(CubeGridEntity parent, MyObjectBuilder_ShipToolBase definition)
+			: base(parent, definition)
+		{
+			m_inventory = new InventoryEntity(definition.Inventory);
+		}
+
+		public ShipToolBaseEntity(CubeGridEntity parent, MyObjectBuilder_ShipToolBase definition, Object backingObject)
+			: base(parent, definition, backingObject)
+		{
+			m_inventory = new InventoryEntity(definition.Inventory, InternalGetContainerInventory());
+		}
+
+		#endregion
+
+		#region "Properties"
+
+		[Category("Ship Tool Base")]
+		[Browsable(false)]
+		[ReadOnly(true)]
+		public InventoryEntity Inventory
+		{
+			get
+			{
+				return m_inventory;
+			}
+		}
+
+		#endregion
+
+		#region "Methods"
+
+		#region "Internal"
+
+		protected Object InternalGetContainerInventory()
+		{
+			try
+			{
+				Object baseObject = BackingObject;
+				Object actualObject = GetActualObject();
+				Object inventory = InvokeEntityMethod(actualObject, ShipToolBaseGetInventoryMethod, new object[] { 0 });
+
+				return inventory;
+			}
+			catch (Exception ex)
+			{
+				LogManager.GameLog.WriteLine(ex);
+				return null;
+			}
+		}
+
+		#endregion
+
+		#endregion
+	}
+}
