c17d650845c91af1d931cddc6047045fd28c15de
chessmaster42 <chessmaster42@gmail.com>
2014-09-04 13:49:05 -0500

    -Added some more base types to the block registry
    -Added the new light properties in game patch 1.046 (read-only for now)

--- SEModAPIInternal/Properties/AssemblyInfo.cs
+++ SEModAPIInternal/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.3.7")]
-[assembly: AssemblyFileVersion("0.2.3.7")]
+[assembly: AssemblyVersion("0.2.3.8")]
+[assembly: AssemblyFileVersion("0.2.3.8")]
